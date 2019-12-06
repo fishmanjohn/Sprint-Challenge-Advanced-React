227831a93e5c9682a6824104851a6efff9f667bc
fishmanjohn <john@emmaandjohnbyvan.com>
2019-12-06 10:24:09 -0800

    sprint finished

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { render } from 'jest';
 import App from './App';
+import Navbar from './components/Navbar';
+import Playerlist from './components/PlayerList'
+import { TestScheduler } from '@jest/core';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it('renders Navbar', ()=>{
+  const navDiv = document.createElement('div');
+  ReactDOM.render(<Navbar/>, navDiv);
+  ReactDOM.unmountComponentAtNode(navDiv)
+})
+
+it('renders list of players', ()=>{
+  const playerDiv = document.createElement('div');
+  ReactDOM.render(<Playerlist/>, playerDiv);
+  ReactDOM.unmountComponentAtNode(playerDiv)
+})
+
